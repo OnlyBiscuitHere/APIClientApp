e5938dc75608e3e0f89cd528cbb17c077ee9d0c8
Benjamin <h8bmf@hotmail.co.uk>
2021-09-13 16:52:13 +0100

    Homework done

---
+++ APIClientApp/APITestApp/Services/OutPostcodeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using APIClientApp;
+
+namespace APITestApp.Services
+{
+    class OutPostcodeService
+    {
+        public RestClient Client;
+        public JObject ResponseContent { get; set; }
+        public string OutwardCode { get; set; }
+        public int StatusCode { get; set; }
+        public OutCodeResponse ResponseObject { get; set; }
+        public OutPostcodeService()
+        {
+            Client = new RestClient { BaseUrl = new Uri(AppConfigReader.BaseUrl) };
+        }
+        public void MakeRequest(string outcode)
+        {
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            request.Resource = $"/outcodes/{outcode}";
+            OutwardCode = outcode;
+            IRestResponse response = Client.Execute(request);
+            ResponseContent = JObject.Parse(response.Content);
+            StatusCode = (int)response.StatusCode;
+            ResponseObject = JsonConvert.DeserializeObject<OutCodeResponse>(response.Content);
+        }
+    }
+}
---
+++ APIClientApp/APITestApp/Test - Sad Path/WhenTheSinglePostcodeServiceIsCalledWithInvalidPostcode.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using APITestApp.Services;
+using System.Threading.Tasks;
+
+namespace APITestApp.Test___Sad_Path
+{
+    public class WhenTheSinglePostcodeServiceIsCalledWithInvalidPostcode
+    {
+        private SinglePostcodeService _singlePostcodeService;
+        [OneTimeSetUp]
+        public async Task OneTimeSetup()
+        {
+            _singlePostcodeService = new SinglePostcodeService();
+            await _singlePostcodeService.MakeRequest("Bad Request");
+        }
+        [Test]
+        public void StatusIs404()
+        {
+            Assert.That(_singlePostcodeService.StatusCode, Is.EqualTo(404));
+        }
+    }
+}
